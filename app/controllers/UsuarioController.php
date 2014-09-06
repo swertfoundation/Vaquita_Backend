e196cb7cbe5ab2b4c6b6c51768477e2dea682513
Leonardo David <leo.david.mm@gmail.com>
2014-09-06 10:50:03 -0400

    Se agrego el controlador de usuarios

---
+++ app/controllers/UsuarioController.php
@@ -0,0 +1,86 @@
+<?php
+
+class UsuarioController extends \BaseController {
+
+	/**
+	 * Retirna la info del perfil actuali logeado en el sistema
+	 * GET /perfil
+	 *
+	 * @return Json Array
+	 */
+	public function getProfile()
+	{
+		//
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 * GET /usuario/create
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 * POST /usuario
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 * GET /usuario/{id}
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 * GET /usuario/{id}/edit
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 * PUT /usuario/{id}
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 * DELETE /usuario/{id}
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
